066aa2dcbd65274ac6abf7f5106f498cfce79d4a
Zhiyu(Garros) Gong <66699813+garroshub@users.noreply.github.com>
2022-12-04 16:45:30 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ with open("style.css") as f:
 # Header 
 st.write('''
 # Zhiyu (Garros) Gong, M.Sc.
-##### *Financial Analytics Professional|Business Scientist|Ph.D.Candidate* 
+##### *Financial Analytics Professional | Business Scientist | Ph.D.Candidate* 
 ''')
 
 image = Image.open('dp.png')
